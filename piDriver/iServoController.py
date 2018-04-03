d2f135d48808911f3d83f44731b16ec9ce990dd6
Asjad Hassan Malick <asjad.malick259@gmail.com>
2018-04-02 19:47:48 -0600

    The new Controller and Pi code
    The controller code will need to be updated to accomodate c# commands, essentially turning commands given by c# to the encoding the pi will accept.

--- piDriver/iServoController.py
+++ piDriver/iServoController.py
@@ -1,25 +1,30 @@
 import socket
 import sys
 
+# Tried doing iServoController.py 1 N,1
+# Expected, roomba makes a sound and rotates back and forth
+# actual, roomba makes 1 beep and 90 degree turn, does this 3 times
+#	also the controller termainl no respond to ctrl+c or +d or +x
+if (len(sys.argv) != 3):
+    print('error is usage: use as \'iServoController.py [piNumber] [commandString]\'')
 
-if(len(sys.argv) != 2):
-	print('error is usage: use as \'iServoController.py [commandString]\'');
-	
 else:
-	command = sys.argv[1];
+    command = sys.argv[2]
+    piNum = sys.argv[1]
 
-	# create a socket object
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-	# get local machine name
-	host = socket.gethostname()                           
-	port = 2225
+    # create a socket object
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # get local machine name
+    # host = socket.gethostname() #was doing for testing chnage back if you want
+    host = "roomba-pi-" + piNum + ".local"
+    port = 2225
 
-	# connection to hostname on the port.
-	s.connect((host, port))                               
-	s.send(command.encode('ascii'))
+    # connection to hostname on the port.
+    s.connect((host, port))
+    s.send(command.encode('ascii'))
 
-	# Receive no more than 1024 bytes
-	msg = s.recv(1024)                                     
+    # Receive no more than 1024 bytes
+    msg = s.recv(1024)
 
-	s.close()
-	print (msg.decode('ascii'))
+    s.close()
+    print(msg.decode('ascii'))
--- piDriver/iServoPi.py
+++ piDriver/iServoPi.py
@@ -1,14 +1,16 @@
 import socket
 import threading
+
 from breezycreate2 import Robot
+
 
 # Define a function for the thread
 # clSocket is the client socket
 # command is a string encoding a command
 
-#"M,num1,num2, num3" encodes a move command to go to position num1, num2 at num3*self.fwdVel speed (0.1 <= num 3 <= 1.0)
-#"N,num" encodes a notify command with  level == num
-#"I,num1,num2,num3" encodes a grid init command, to say robot is at position num1, num2 with heading num3
+# "M,num1,num2, num3" encodes a move command to go to position num1, num2 at num3*self.fwdVel speed (0.1 <= num 3 <= 1.0)
+# "N,num" encodes a notify command with  level == num
+# "I,num1,num2,num3" encodes a grid init command, to say robot is at position num1, num2 with heading num3
 class myThread(threading.Thread):
     def __init__(self, sock, com, iServo):
         threading.Thread.__init__(self)
@@ -17,40 +19,40 @@ class myThread(threading.Thread):
         self.bot = iServo
 
     def run(self):
-        if(self.command[0] == 'M'):
-            #gridMoveTo
-            cmdList = self.command.split(',');
-            x = float(cmdList[1]);
-            y = float(cmdList[2]);
-            spd = float(cmdList[3]);
-            curSpd = self.bot.getFWDVel;
-            self.bot.setFWDVel(crSpd*spd);
-            self.bot.gridMoveTo(x,y);
-            self.bot.setFWDVel = curSpd;
-        elif(self.command[0] == 'N'):
-            #Notify
-            cmdList = self.command.split(',');
-            lvl = int(cmdList[1]);
-            self.bot.notify(lvl);
-        elif(self.command[0] == 'I'):
-            #Re-initialize
-            cmdList = self.command.split(',');
-            newX = float(cmdList[1]);
-            newY = float(cmdList[2]);
-            newH = float(cmdList[3]);
-            self.bot.initGrid(newX, newY, newH);
-            
-        msg = 'Command:' + self.command + 'complete' + '\nBot: X: '
-        + self.bot.x + " Y: " + self.bot.y+" [H]: " +self.heading;
+        if (self.command[0] == 'M'):
+            # gridMoveTo
+            cmdList = self.command.split(',')
+            x = float(cmdList[1])
+            y = float(cmdList[2])
+            spd = float(cmdList[3])
+            curSpd = self.bot.getFWDVel()
+            self.bot.setFWDVel(curSpd * spd)
+            self.bot.gridMoveTo(x, y)
+            self.bot.setFWDVel = curSpd
+        elif (self.command[0] == 'N'):
+            # Notify
+            cmdList = self.command.split(',')
+            lvl = int(cmdList[1])
+            self.bot.notify(lvl)
+        elif (self.command[0] == 'I'):
+            # Re-initialize
+            cmdList = self.command.split(',')
+            newX = float(cmdList[1])
+            newY = float(cmdList[2])
+            newH = float(cmdList[3])
+            self.bot.initGrid(newX, newY, newH)
 
-        self.socket.send(msg.encode('ascii'));
-        self.socket.close();
+        msg = "Command: " + str(self.command) + " complete" + "\nBot: X: " + str(self.bot.x) + " Y: " + str(self.bot.y) + " [H]: " + str(self.bot.heading)
 
-bot = Robot();
+        self.socket.send(msg.encode('ascii'))
+        self.socket.close()
+
+
+bot = Robot()
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # get local machine name
-#host = socket.gethostname()
+# host = socket.gethostname()
 host = "roomba-pi-1.local"
 port = 2225
 
@@ -64,15 +66,15 @@ run = True
 
 while run:
     # establish a connection
-    clientsocket, addr = serversocket.accept();
-    msgPacket = clientsocket.recv(1024);
-    message = msgPacket.decode('ascii');
+    clientsocket, addr = serversocket.accept()
+    msgPacket = clientsocket.recv(1024)
+    message = msgPacket.decode('ascii')
 
     if (message == 'q'):
-        run = False;
+        run = False
         err = 'Closing server: ' + str(host) + "\r\n"
         clientsocket.send(err.encode('ascii'))
-        clientsocket.close();
+        clientsocket.close()
 
     else:
         try:
@@ -82,6 +84,6 @@ while run:
         except:
             err = 'Could not start request from: ' + str(addr) + "\r\n"
             clientsocket.send(err.encode('ascii'))
-            clientsocket.close();
+            clientsocket.close()
 
 serversocket.close()
